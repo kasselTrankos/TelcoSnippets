6da9a6c739c9044a6ad27f478981781255efeab3
kasselTrankos <alvaro.touzon@gmail.com>
2016-11-07 23:24:58 +0100

    generate

--- TelcoSnippets.py
+++ TelcoSnippets.py
@@ -1,33 +1,14 @@
 import sublime, sublime_plugin, os, locale, sys, re
-from xml.dom import minidom
-from xml.etree import ElementTree
-import ntpath
-import subprocess
-from subprocess import Popen, PIPE, STDOUT
-from sys import version_info
-import datetime
-
 
 class TelcoSnippetsCommand():
+	def init(self):
+		sublime.message_dialog('jhla')
+	def load_content(self, path_):
+		sublime.message_dialog(path_)
+		return open(path_, 'r+')
 
-class TelcoCommand(sublime_plugin.WindowCommand, FiddlerInjectorCommand):
+class TelcoSpinnerCommand(sublime_plugin.WindowCommand, TelcoSnippetsCommand):
+
 	def run(self, paths=[], parameters=None):
-		sublime.message_dialog('<::::::jol::::>'+paths[0]+'----')
-		isCGT, name = self.getCnt_cgt_info(paths[0])
-		fname = self.window.active_view().file_name()
-		if fname == None:
-			fname = ""
-		if isCGT:
-			sublime.message_dialog('<::::::jol::::>CGT----')
-		else:
-			sublime.message_dialog('<::::::jol::::>CNT----')
-		def done(cntName):
-				self.create_fiddler(paths[0], parameters, cntName, isCGT, name)
-		if parameters is None:
-			parameters = get_setting('parameters', [])
-
-		if(isCGT):
-			self.window.show_input_panel(
-            "Name of CNT is: ", fname, done, None, None)
-		else:
-			self.create_fiddler(paths[0], parameters, name, isCGT, name)
+		content = self.load_content(paths[0])
+		sublime.message_dialog(paths[0])
