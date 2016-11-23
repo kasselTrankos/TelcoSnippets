404307e98c1c5b373e3bd9e8470c62969280c5df
kasselTrankos <alvaro.touzon@gmail.com>
2016-11-23 22:15:06 +0100

    more efficient reader

--- snippets/spinner.py
+++ snippets/spinner.py
@@ -1,8 +1,11 @@
-import os, re, sublime, os, json
+import os, re, sublime, os, json, sys
+from ..rspec.rspec_print import rspec_print
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'pyesprima', 'pyesprima'))
 import logging
 from .scripts import openSpinner
 from .scripts import closeSpinner
 from .scripts import initVarsSpinner
+import pyesprima
 
 class Spinner:
 	controllers = []
@@ -16,8 +19,9 @@ class Spinner:
 		self.localize(content['body'])
 		return content
 	def localize(self, body):
-		i = 0;
+		i = 0
 		for elm in body:
+			rspec_print('CONTROLLER'+str(elm))
 			ControllerName, arguments, bodyFuncs, bodyParams, serviceFuncs, serviceName = self.getController(elm['expression'])
 
 			if ControllerName!=False and  serviceName!=False:
@@ -46,6 +50,7 @@ class Spinner:
 		gotCloseSpinner = False
 		gotOpenSpinner = False
 		for item in bodyFuncs:
+			rspec_print('BODY:::::::.'+str(item))
 			gotOpenSpinner = self.functionForService(item, 'isOpenSpinner')
 			gotCloseSpinner = self.functionForService(item, 'closeSpinner')
 		if gotCloseSpinner==False:
@@ -54,7 +59,8 @@ class Spinner:
 			bodyFuncs.append(openSpinner.script)
 
 	def getController(self, bodyElm):
-		if bodyElm['type']=='CallExpression' and bodyElm['callee']['property']['name']=='controller':
+		rspec_print('BODYE LM '+str(bodyElm))
+		if bodyElm and bodyElm['type']=='CallExpression' and bodyElm['callee']['property']['name']=='controller':
 			arguments, bodyFuncs, serviceName = self.getArguments(bodyElm['arguments'][1]['elements'])
 			serviceFuncs = []
 			for func in bodyFuncs['body']['body']:
@@ -75,9 +81,17 @@ class Spinner:
 		return [arguments, init, serviceName]
 
 	def functionForService(self, function, serviceName, arr=[], d=False):
+		if function==None:
+			return 0
 		if d==False:
 			d = function
-		for k,v in d.__dict__.items():
+		if isinstance(d, pyesprima.jsdict):
+			o = d.__dict__
+		elif isinstance(d, dict):
+			o = d
+
+
+		for k,v in o.items():
 			if isinstance(v, dict):
 				self.functionForService(function, serviceName, arr, d[k])
 			elif isinstance(v, list):
