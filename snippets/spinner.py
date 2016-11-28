84d22153e3ab8535b74bc11e3d2b9de3f11d58cc
kasselTrankos <alvaro.touzon@gmail.com>
2016-11-28 22:30:30 +0100

    added best json path

--- snippets/spinner.py
+++ snippets/spinner.py
@@ -1,5 +1,14 @@
 import os, re, sublime, os, json, sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'pyesprima', 'pyesprima'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'python-jsonpath-rw'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'six'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'ply'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'ObjectPath'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'..', 'jmespath.py'))
+
+import jmespath
+#from jsonpath_rw import jsonpath, parse
+#from objectpath import *
 from ..rspec.rspec_print import rspec_print
 import logging
 from .scripts import openSpinner
@@ -17,9 +26,21 @@ class Spinner:
 		formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 		hdlr.setFormatter(formatter)
 		self.logger.addHandler(hdlr)
-		self.localize(content['body'])
+		self.localize(content)
 		return content
 	def localize(self, body):
-		funcInit = findFunction('init', body)
+		#rspec_print('instance of'+{'foo': {'bar': 'baz'})
+		js = json.loads(json.dumps(body, default=lambda o: o.__dict__,
+            sort_keys=True, indent=1))
+		#body[?type=='ExpressionStatement'].expression.arguments[-1].elements[-1].body.body[]
+		#.arguments[-1].elements[-1].body.body[][?expression.left.property.name=='init'].expression.right[?type=='FunctionExpression'].body[?type=='BlockStatement'].body
+		results = jmespath.search("body[?type=='ExpressionStatement'].expression.arguments[-1][?type=='ArrayExpression'].elements[-1][?type=='FunctionExpression']", js)
+		#results = [match.value for match in parse("$[*].`this`('ExpressionStatement')").find(body)]
+		rspec_print('FOND JSON '+str(results))
+		#self.logger.info(js);
+		#funcInit = findFunction('init', body)
+		#tree=Tree(str(body))
+		#r = tree.execute("$..type")
+		#rspec_print('RESULTADO ES '+str(r))
 
 
