50929f3a0bd31df3bb0364274a7996345589a200
kasselTrankos <alvaro.touzon@gmail.com>
2016-11-29 13:34:43 +0100

    revisted in windows

--- TelcoSnippets.py
+++ TelcoSnippets.py
@@ -1,18 +1,17 @@
 import sublime,imp, sublime_plugin, os, locale, sys, re
 from .snippets.spinner import Spinner
 from .snippets.angular import Angular
-from .pyjsparser.pyjsparser import PyJsParser
-#from .slimit.src.slimit.parser import Parser
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pyesprima', 'pyesprima'))
 import pyesprima
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+
 CODE_DIRS = [
   'plugin_helpers',
   'rspec',
 ]
 sys.path += [BASE_PATH] + [os.path.join(BASE_PATH, f) for f in CODE_DIRS]
-# =======
 # reload plugin files on change
 if 'plugin_helpers.reloader' in sys.modules:
   imp.reload(sys.modules['plugin_helpers.reloader'])
@@ -27,10 +26,8 @@ class TelcoSnippetsCommand():
 		return open(path_, 'r+').read()
 
 class TelcoSpinnerCommand(sublime_plugin.WindowCommand, TelcoSnippetsCommand):
-
 	def run(self, paths=[], parameters=None):
 		spinner = Spinner()
-		p = PyJsParser()
 		a = Angular()
 		content = self.load_content(paths[0])
 		#lexer = JsLexer()
